ed4a7a4a3b31ee003fab25d8eb4311680414eddb
TheCaptain989 <thecaptain989@protonmail.com>
2020-09-26 19:14:14 -0500

    New preview build (#36)
    * Prep to not hard-code tag in Dockerfile
    * Removed Microbadger.  That thing is just flaky.
    * Reworked Title composition
    * Create striptracks-fre-debug.sh
    * Update striptracks.sh
    * Removed dependency on printf
    * Corrected some debug output
    * Now compatible with newer mkvmerge (resolves #37)
    * resolves #27 
    
    Hacked my way through using jq.  I still want to remove awk script for pure bash/jq, but this will at least let the script work.

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-A Radarr/Sonarr Docker container with a script that uses mkvmerge to automatically strip out unwanted audio and subtitle streams, keeping only the desired languages. Chapters, if they exist, are preserved. The Title attribute in the MKV is sset to the video title plus year (ex: `The Sting (1973)`).
+A Radarr/Sonarr Docker container with a script that uses mkvmerge to automatically strip out unwanted audio and subtitle streams, keeping only the desired languages.
+Chapters, if they exist, are preserved. The Title attribute in the MKV is set to the movie title plus year (ex: `The Sting (1973)`) or the series title plus episode information (ex: `Happy! 01x01 - What Smiles Are For`).
 
 **One unified script works in both Radarr and Sonarr.  Both containers are auto-built when the script is updated on Github, or when the source container is updated.**
+>**NOTE:** These containers support Linux OSes only.
 
 Radarr container info:
-[![](https://images.microbadger.com/badges/image/thecaptain989/radarr.svg)](https://microbadger.com/images/thecaptain989/radarr "Get your own image badge on microbadger.com")
-[![](https://images.microbadger.com/badges/version/thecaptain989/radarr.svg)](https://microbadger.com/images/thecaptain989/radarr "Get your own version badge on microbadger.com")
+![Docker Image Size (latest by date)](https://img.shields.io/docker/image-size/thecaptain989/radarr)
+![MicroBadger Layers](https://img.shields.io/microbadger/layers/thecaptain989/radarr)
 ![Docker Pulls](https://img.shields.io/docker/pulls/thecaptain989/radarr "Radarr Container Pulls")   
 Sonarr container info:
-[![](https://images.microbadger.com/badges/image/thecaptain989/sonarr.svg)](https://microbadger.com/images/thecaptain989/sonarr "Get your own image badge on microbadger.com")
-[![](https://images.microbadger.com/badges/version/thecaptain989/sonarr.svg)](https://microbadger.com/images/thecaptain989/sonarr "Get your own version badge on microbadger.com")
+![Docker Image Size (latest by date)](https://img.shields.io/docker/image-size/thecaptain989/sonarr)
+![MicroBadger Layers](https://img.shields.io/microbadger/layers/thecaptain989/sonarr)
 ![Docker Pulls](https://img.shields.io/docker/pulls/thecaptain989/sonarr "Sonarr Container Pulls")
 
 # Installation
@@ -18,37 +20,34 @@ Sonarr container info:
   `docker pull thecaptain989/radarr:latest`   OR  
   `docker pull thecaptain989/sonarr:latest`   
 
->**NOTE:** These containers support Linux OSes only.
-
 2. Configure the Docker container with all the port, volume, and environment settings from the *original container documentation* here:  
    **[linuxserver/radarr](https://hub.docker.com/r/linuxserver/radarr "Docker container")**  
    **[linuxserver/sonarr](https://hub.docker.com/r/linuxserver/sonarr "Docker container")**
 
 3. After all of the above configuration is complete, to use mkvmerge:  
    1. Configure a custom script from the Settings->Connect screen and type the following in the **Path** field:  
-
       **`/usr/local/bin/striptracks.sh`**  
 
    
    2. Add the codes for the audio and subtitle languages you want to keep as **Arguments** (details in the [Syntax](./README.md#syntax) section below):
-
       <ins>Suggested Example</ins>  
       **`:eng:und :eng`**
 
 ## Usage
->**NOTE:** See the [Preview Branch](./README.md#preview-branch) section below for important differences to these instructions for v3 builds.   
+>**NOTE:** See the [Preview Branch](./README.md#preview-branch) section below for important differences to these instructions for v3 builds.
 
 The source video can be any mkvtoolnix supported video format. The output is an MKV file with the same name.
 
 If you've configured the Radarr/Sonarr Recycle Bin path correctly, the original video will be moved there.  
->![warning24] **NOTE:** If you have *not* configured the Recycle Bin, the original video file will be deleted/overwritten and permanently lost.
+![warning24] **NOTE:** If you have *not* configured the Recycle Bin, the original video file will be deleted/overwritten and permanently lost.
 
 ### Syntax
 The script accepts two arguments and one option in the **Arguments** field:
 
 `[-d] <audio_languages> <subtitle_languages>`
 
-The arguments are language codes in [ISO639-2](https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes "List of ISO 639-2 codes") format. These are three letter abbreviations prefixed with a colon ':', such as:
+The arguments are language codes in [ISO639-2](https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes "List of ISO 639-2 codes") format.
+These are three letter abbreviations prefixed with a colon ':', such as:
 
 * :eng
 * :fre
@@ -59,7 +58,8 @@ The arguments are language codes in [ISO639-2](https://en.wikipedia.org/wiki/Lis
 Multiple codes may be concatenated, such as `:eng:spa` for both English and Spanish.  
 
 It is suggested to use `:eng:und :eng` if you are unsure of what to choose. This will keep English and Undetermined audio and English subtitles, if they exist.
->**NOTE:** The script is smart enough to not remove the last audio track. This way you don't have to specify every possible language if you are importing a foreign film, for example.
+>**NOTE:** The script is smart enough to not remove the last audio track. This way you don't have to specify every possible language if you are importing a
+foreign film, for example.
 
 The `-d` option enables debug logging.
 
@@ -76,7 +76,6 @@ The only events/notification triggers that have been tested are **On Download** 
 
 ![striptracks](https://raw.githubusercontent.com/TheCaptain989/radarr-striptracks/master/images/striptracks.png "Radarr/Sonarr custom script settings")
 
-
 ## Logs
 A log file is created for the script activity called:
 
@@ -92,10 +91,10 @@ Log rotation is performed with 5 log files of 512KB each being kept.
 ___
 
 ## Preview Branch
->![warning] **Warning: Unstable Releases** ![warning]   
->The Preview branch is for the v3 unstable releases (Aphrodite and Phantom) of Radarr and Sonarr. I cannot guarantee these releases are stable and might perform breaking changes at any time.   
+>![warning] **Warning: Unstable Releases** ![warning]  
+>The Preview branch is for the v3 unstable releases (Aphrodite and Phantom) of Radarr and Sonarr. I cannot guarantee these releases are stable.
 
-<ins>Important differences for Preview Branch</ins>   
+<ins>Important differences for Preview branch</ins>
 ### Preview Installation
 Substitute the following steps for those noted in the [Installation](./README.md#installation) section above.
 1. Pull your selected container ([thecaptain989/radarr](https://hub.docker.com/r/thecaptain989/radarr "TheCaptain989's Radarr container") or [thecaptain989/sonarr](https://hub.docker.com/r/thecaptain989/sonarr "TheCaptain989's Sonarr container")) from Docker Hub:  
@@ -110,22 +109,17 @@ Substitute the following steps for those noted in the [Installation](./README.md
 
       **`/usr/local/bin/striptracks-eng.sh`**  
 
-      <ins>This script uses the following options, which keep English audio and subtitles only!</ins>  
-      **`:eng:und :eng`**
+      <ins>This is a wrapper script uses the following options, which keep English audio and subtitles only!</ins>  
+      `:eng:und :eng`
 
-      *For any other combinations of audio and subtitles you must either use one of the [included wrapper scripts](./README.md#included-wrapper-scripts) or create a custom script with the codes for the languages you want to keep.  See the [Syntax](./README.md#syntax) section above.*
-
-### Example Wrapper Script
->**NOTE:** The **Arguments** field for Custom Scripts was removed in Radarr and Sonarr v3 due to security concerns. To support options with this version and later, a wrapper script can be manually created that will call *striptracks.sh* with the required arguments.
-
-To use the last [example](./README.md#examples) above, create and save the following text in a file called `wrapper.sh` and then use that in the **Path** field in place of `striptracks-eng.sh` mentioned in the [Preview Installation](./README.md#preview-installation) section above.
-```
-#!/bin/bash
-
-. /usr/local/bin/striptracks.sh :eng:kor:jpn :eng:spa
-```
+      *For any other combinations of audio and subtitles you **must** either use one of the [included wrapper scripts](./README.md#included-wrapper-scripts) or
+      create a custom script with the codes for the languages you want to keep.  See the [Syntax](./README.md#syntax) section above.
+      Do not put `striptracks.sh` in the **Path** field!*
 
 ### Included Wrapper Scripts
+>**NOTE:** The **Arguments** field for Custom Scripts was removed in Radarr and Sonarr v3 due to security concerns. To support options with this version and later,
+a wrapper script can be manually created that will call *striptracks.sh* with the required arguments.
+
 For your convenience, several wrapper scripts are included in the Docker container in the `/usr/local/bin/` directory.  
 You may use any of these scripts in place of the `striptracks-eng.sh` mentioned in the [Preview Installation](./README.md#preview-installation) section above.
 
@@ -137,6 +131,15 @@ striptracks-fre.sh         # Keep French audio and subtitles
 striptracks-ger.sh         # Keep German audio and subtitles
 striptracks-dut.sh         # Keep Dutch audio and subtitles
 ```
+
+### Example Wrapper Script
+To configure the last entry from the [Examples](./README.md#examples) section above, create and save a file called `wrapper.sh` containing the following text:
+```
+#!/bin/bash
+
+. /usr/local/bin/striptracks.sh :eng:kor:jpn :eng:spa
+```
+Then put `/usr/local/bin/wrapper.sh` in the **Path** field in place of `/usr/local/bin/striptracks-eng.sh` mentioned in the [Preview Installation](./README.md#preview-installation) section above.
 
 ### Preview Triggers
 The only events/notification triggers that have been tested are **On Import** and **On Upgrade**
--- striptracks.sh
+++ striptracks.sh
@@ -25,7 +25,9 @@
 #  3 - no subtitle language specified on command line
 #  4 - mkvmerge not found
 #  5 - specified video file not found
+#  6 - unable to rename video to temp video
 # 10 - remuxing completed, but no output file found
+# 20 - general error
 
 ### Variables
 export striptracks_script=$(basename "$0")
@@ -41,12 +43,14 @@ if [[ "${striptracks_type,,}" = "radarr" ]]; then
   export striptracks_api_endpoint="movie"
   export striptracks_json_quality_root=".movieFile"
   export striptracks_video_type="movie"
+  export striptracks_title="$radarr_movie_title ($radarr_movie_year)"
 else
   if [[ "${striptracks_type,,}" = "sonarr" ]]; then
     export striptracks_video="$sonarr_episodefile_path"
     export striptracks_api_endpoint="episodefile"
     export striptracks_json_quality_root=""
     export striptracks_video_type="series"
+    export striptracks_title="$sonarr_series_title $(numfmt --format "%02f" $sonarr_episodefile_seasonnumber)x$(numfmt --format "%02f" $sonarr_episodefile_episodenumbers) - $sonarr_episodefile_episodetitles"
   fi
 fi
 export striptracks_api="Rescan${striptracks_video_type^}"
@@ -58,12 +62,12 @@ export striptracks_video_id="${!striptracks_video_idname}"
 export striptracks_eventtype="${striptracks_type,,}_eventtype"
 export striptracks_tempvideo="${striptracks_video}.tmp"
 export striptracks_newvideo="${striptracks_video%.*}.mkv"
-export striptracks_title=$(basename "${striptracks_video%.*}")
 export striptracks_db="/config/${striptracks_type,,}.db"
 if [ ! -f "$striptracks_db" ]; then
   striptracks_db=/config/nzbdrone.db
 fi
 export striptracks_recyclebin=$(sqlite3 $striptracks_db 'SELECT Value FROM Config WHERE Key="recyclebin"')
+RET=$?; [ "$RET" != 0 ] && >&2 echo "WARNING[$RET]: Unable to read recyclebin information from database \"$striptracks_db\""
 
 ### Functions
 function usage {
@@ -122,7 +126,7 @@ function get_video_info {
   [ $striptracks_debug -eq 1 ] && echo "Debug|Getting video information for $striptracks_api_endpoint '$striptracks_api_endpoint_id'. Calling ${striptracks_type^} API using GET and URL 'http://$striptracks_bindaddress:$striptracks_port$striptracks_urlbase/api/$striptracks_api_endpoint/$striptracks_api_endpoint_id?apikey=(removed)'" | log
   RESULT=$(curl -s -H "Content-Type: application/json" \
     -X GET http://$striptracks_bindaddress:$striptracks_port$striptracks_urlbase/api/$striptracks_api_endpoint/$striptracks_api_endpoint_id?apikey=$striptracks_apikey)
-  [ $striptracks_debug -eq 1 ] && echo "Debug|API returned: $RESULT" | log
+  [ $striptracks_debug -eq 1 ] && echo "API returned: $RESULT" | awk '{print "Debug|"$0}' | log
   if [ "$(echo $RESULT | jq -crM .path)" != "null" ]; then
     local RET=0
   else
@@ -137,7 +141,7 @@ function rescan {
   [ $striptracks_debug -eq 1 ] && echo "Debug|Forcing rescan of $striptracks_json_key '$striptracks_video_id', try #$i. Calling ${striptracks_type^} API '$striptracks_api' using POST and URL 'http://$striptracks_bindaddress:$striptracks_port$striptracks_urlbase/api/command?apikey=(removed)'" | log
   RESULT=$(curl -s -d "{name: '$striptracks_api', $striptracks_json_key: $striptracks_video_id}" -H "Content-Type: application/json" \
     -X POST http://$striptracks_bindaddress:$striptracks_port$striptracks_urlbase/api/command?apikey=$striptracks_apikey)
-  [ $striptracks_debug -eq 1 ] && echo "Debug|API returned: $RESULT" | log
+  [ $striptracks_debug -eq 1 ] && echo "API returned: $RESULT" | awk '{print "Debug|"$0}' | log
   JOBID="$(echo $RESULT | jq -crM .id)"
   if [ "$JOBID" != "null" ]; then
     local RET=0
@@ -153,7 +157,7 @@ function check_rescan {
     [ $striptracks_debug -eq 1 ] && echo "Debug|Checking job $JOBID completion, try #$i. Calling ${striptracks_type^} API using GET and URL 'http://$striptracks_bindaddress:$striptracks_port$striptracks_urlbase/api/command/$JOBID?apikey=(removed)'" | log
     RESULT=$(curl -s -H "Content-Type: application/json" \
       -X GET http://$striptracks_bindaddress:$striptracks_port$striptracks_urlbase/api/command/$JOBID?apikey=$striptracks_apikey)
-    [ $striptracks_debug -eq 1 ] && echo "Debug|API returned: $RESULT" | log
+    [ $striptracks_debug -eq 1 ] && echo "API returned: $RESULT" | awk '{print "Debug|"$0}' | log
     if [ "$(echo $RESULT | jq -crM .status)" = "completed" ]; then
       local RET=0
       break
@@ -228,10 +232,49 @@ if [ ! -f "$striptracks_video" ]; then
   exit 5
 fi
 
+#### BEGIN MAIN
 FILESIZE=$(numfmt --to iec --format "%.3f" $(stat -c %s "$striptracks_video"))
 MSG="Info|${striptracks_type^} event: ${!striptracks_eventtype}, Video: $striptracks_video, Size: $FILESIZE, AudioKeep: $1, SubsKeep: $2"
 echo "$MSG" | log
-echo "" | awk -v Debug=$striptracks_debug \
+
+# Rename the original video file to a temporary name
+[ $striptracks_debug -eq 1 ] && echo "Debug|Renaming: \"$striptracks_video\" to \"$striptracks_tempvideo\"" | log
+mv -f "$striptracks_video" "$striptracks_tempvideo" | log
+RET=$?; [ "$RET" != 0 ] && {
+  MSG="ERROR[$RET]: Unable to rename video: \"$striptracks_video\" to temp video: \"$striptracks_tempvideo\". Halting."
+  echo "$MSG" | log
+  >&2 echo "$MSG"
+  exit 6
+}
+
+# Read in the output of mkvmerge info extraction
+[ $striptracks_debug -eq 1 ] && echo "Debug|Executing: /usr/bin/mkvmerge -J \"$striptracks_tempvideo\"" | log
+JSON=$(/usr/bin/mkvmerge -J "$striptracks_tempvideo")
+RET=$?; [ "$RET" != 0 ] && {
+  MSG="ERROR[$RET]: Error executing mkvmerge."
+  echo "$MSG" | log
+  >&2 echo "$MSG"
+}
+
+# This and the modified AWK script are a hack, and I know it.  JQ is crazy hard to learn, BTW.
+# Mimic the mkvmerge --identify-verbose option that has been deprecated
+JSON_PROCESSED=$(echo $JSON | jq -jcrM '
+( if (.chapters | .[] | .num_entries) then
+    "Chapters: \(.chapters | .[] | .num_entries) entries\n"
+  else
+    ""
+  end
+),
+( .tracks |
+  .[] |
+  ( "Track ID \(.id): \(.type) (\(.codec)) [",
+    ( [.properties | to_entries |.[] | "\(.key):\(.value | tostring | gsub(" "; "\\s"))"] | join(" ")),
+    "]\n" )
+)
+')
+[ $striptracks_debug -eq 1 ] && echo "$JSON_PROCESSED" | awk '{print "Debug|"$0}' | log
+
+echo "$JSON_PROCESSED" | awk -v Debug=$striptracks_debug \
 -v OrgVideo="$striptracks_video" \
 -v TempVideo="$striptracks_tempvideo" \
 -v MKVVideo="$striptracks_newvideo" \
@@ -242,42 +285,29 @@ BEGIN {
   MKVMerge="/usr/bin/mkvmerge"
   FS="[\t\n: ]"
   IGNORECASE=1
-  if (match(Title,/[a-zA-Z0-9]- /)) {
-    Arr[1]=substr(Title,1,RSTART)
-    Arr[2]=substr(Title,RSTART+RLENGTH-1)
-    Title=Arr[1]":"Arr[2]
-  }  # mawk does not have gensub function
-
-  if (Debug) print "Debug|Renaming: \""OrgVideo"\" to \""TempVideo"\""
-  Result=system("mv \""OrgVideo"\" \""TempVideo"\"")
-  if (Result) {
-    print "Error|"Result" renaming \""OrgVideo"\"" > "/dev/stderr"
-    exit
-  }
-
-  # Read in the output of mkvmerge
-  exe=MKVMerge" --identify-verbose \""TempVideo"\""
-  while ((exe | getline Line) > 0) {
-    if (Debug) print "Debug|"Line
-    FieldCount=split(Line, Fields)
-    if (Fields[1]=="Track") {
-      NoTr++
-      Track[NoTr, "id"]=Fields[3]
-      Track[NoTr, "typ"]=Fields[5]
-      if (Fields[6]~/^\(/) {
-        Track[NoTr, "code"]=substr(Line,1,match(Line,/\)/))
-        sub(/^[^\(]+/,"",Track[NoTr, "code"])
-      }
-      if (Track[NoTr, "typ"]=="video") VidCnt++
-      if (Track[NoTr, "typ"]=="audio") AudCnt++
-      if (Track[NoTr, "typ"]=="subtitles") SubsCnt++
-      for (i=6; i<=FieldCount; i++) {
-        if (Fields[i]=="language") Track[NoTr, "lang"]=Fields[++i]
-      }
-    } else if (Fields[1]=="Chapters") {
-      Chapters=Fields[3]
+}
+/^Track ID/ {
+  FieldCount=split($0, Fields)
+  if (Fields[1]=="Track") {
+    NoTr++
+    Track[NoTr, "id"]=Fields[3]
+    Track[NoTr, "typ"]=Fields[5]
+    if (Fields[6]~/^\(/) {
+      Track[NoTr, "code"]=substr(Line,1,match(Line,/\)/))
+      sub(/^[^\(]+/,"",Track[NoTr, "code"])
+    }
+    if (Track[NoTr, "typ"]=="video") VidCnt++
+    if (Track[NoTr, "typ"]=="audio") AudCnt++
+    if (Track[NoTr, "typ"]=="subtitles") SubsCnt++
+    for (i=6; i<=FieldCount; i++) {
+      if (Fields[i]=="language") Track[NoTr, "lang"]=Fields[++i]
     }
   }
+}
+/^Chapters/ {
+  Chapters=$3
+}
+END {
   if (!NoTr) { print "Error|No tracks found in \""TempVideo"\"" > "/dev/stderr"; exit }
   if (!AudCnt) AudCnt=0; if (!SubsCnt) SubsCnt=0
   print "Info|Original tracks: "NoTr" (audio: "AudCnt", subtitles: "SubsCnt")"
@@ -339,6 +369,8 @@ BEGIN {
   Result=system("nice "MKVMerge" --title \""Title"\" -q -o \""MKVVideo"\" "CommandLine" \""TempVideo"\"")
   if (Result>1) print "Error|"Result" remuxing \""TempVideo"\"" > "/dev/stderr"
 }' | log
+
+#### END MAIN
 
 # Check for script completion and non-empty file
 if [ -s "$striptracks_newvideo" ]; then
@@ -403,7 +435,7 @@ if [ -f "$striptracks_arr_config" ]; then
                   [ $striptracks_debug -eq 1 ] && echo "Debug|Updating quality to '$(echo $ORGQUALITY | jq -crM .quality.name)'. Calling ${striptracks_type^} API using PUT and URL 'http://$striptracks_bindaddress:$striptracks_port$striptracks_urlbase/api/$striptracks_api_endpoint/$striptracks_video_id?apikey=(removed)'" | log
                   RESULT=$(curl -s -d "$(echo $RESULT | jq -crM "${striptracks_json_quality_root}.quality=$ORGQUALITY")" -H "Content-Type: application/json" \
                     -X PUT http://$striptracks_bindaddress:$striptracks_port$striptracks_urlbase/api/$striptracks_api_endpoint/$striptracks_video_id?apikey=$striptracks_apikey)
-                  [ $striptracks_debug -eq 1 ] && echo "Debug|API returned: $RESULT" | log
+                  [ $striptracks_debug -eq 1 ] && echo "API returned: $RESULT" | awk '{print "Debug|"$0}' | log
                   if [ "$(echo $RESULT | jq -crM ${striptracks_json_quality_root}.quality.quality.name)" = "Unknown" ]; then
                     MSG="Warn|Unable to update ${striptracks_type^} $striptracks_api_endpoint '$striptracks_title' to quality '$(echo $ORGQUALITY | jq -crM .quality.name)'"
                     echo "$MSG" | log
