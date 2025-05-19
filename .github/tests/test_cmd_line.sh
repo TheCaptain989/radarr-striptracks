6f44422d94d2dff64dfe8ca206f9c1cd4a71eb51
TheCaptain989 <thecaptain989@protonmail.com>
2025-05-19 18:45:29 +0000

    Fix BuildImage

--- .github/tests/test_cmd_line.sh
+++ .github/tests/test_cmd_line.sh
@@ -15,22 +15,22 @@ test_cmd_options_require_argument() {
   assert_status_code 1 "process_command_line --config" &&
 }
 
-test_cmd_unknown_option(){
+test_cmd_unknown_option() {
   assert_status_code 20 "process_command_line --will-fail"
 }
 
-test_cmd_invalid_audio_option(){
+test_cmd_invalid_audio_option() {
   assert_status_code 2 "process_command_line --audio eng"
 }
 
-test_cmd_invalid_subs_option(){
+test_cmd_invalid_subs_option() {
   assert_status_code 3 "process_command_line --subtitles eng"
 }
 
-test_cmd_dup_options_combo_1(){
+test_cmd_dup_options_combo_1() {
   assert_equals "Warning|Both positional and named arguments set for audio. Using :org" "process_command_line :eng -a :org 2>&1"
 }
 
-test_cmd_dup_options_combo_2(){
+test_cmd_dup_options_combo_2() {
   assert_equals "Warning|Both positional and named arguments set for subtitles. Using :org" "process_command_line :eng -s :org 2>&1"
 }
