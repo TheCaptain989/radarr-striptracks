eddb16e67943421a05bb85c47dade33d630df821
TheCaptain989 <thecaptain989@protonmail.com>
2025-05-22 18:08:58 +0000

    Edit unit test job

--- .github/tests/test_08_remux_video.sh
+++ .github/tests/test_08_remux_video.sh
@@ -24,7 +24,7 @@ setup() {
 test_get_media_info() {
   process_command_line -a :eng -f "$test_video1"
   get_mediainfo "$test_video1"
-  assert_equals "true" "$(echo $striptracks_json | jq -crM '.container.supported')"
+  assert_equals "true" "$(echo $striptracks_json | jq --unbuffered -crM '.container.supported')"
 }
 
 test_remux_video() {
@@ -45,7 +45,7 @@ test_remove_all_subtitles() {
   remux_video
   set_perms_and_owner
   replace_original_video
-  assert_equals "" "$(/usr/bin/mkvmerge -J "$striptracks_video" | jq '.tracks[] | select(.type == "subtitles")')"
+  assert_equals "" "$(mkvmerge -J "$striptracks_video" | jq -scrM '.tracks[] | select(.type == "subtitles")')"
 }
 
 teardown_suite() {
