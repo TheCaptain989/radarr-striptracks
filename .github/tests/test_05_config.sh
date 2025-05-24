229c2d02caa2be9a409b64a571706ac6cd7f1bcf
TheCaptain989 <thecaptain989@protonmail.com>
2025-05-24 01:18:40 +0000

    New unit test jobs using a container

--- .github/tests/test_05_config.sh
+++ .github/tests/test_05_config.sh
@@ -7,26 +7,27 @@ setup_suite() {
   source ../../root/usr/local/bin/striptracks.sh
   export radarr_eventtype="Import"
   initialize_variables
-  export striptracks_arr_config="config.xml"
-  cat >$striptracks_arr_config <<EOF
-<Config>
-  <BindAddress>*</BindAddress>
-  <Port>7878</Port>
-  <SslPort>9898</SslPort>
-  <EnableSsl>False</EnableSsl>
-  <LaunchBrowser>True</LaunchBrowser>
-  <ApiKey>0123456789ABCDEF</ApiKey>
-  <AuthenticationMethod>Forms</AuthenticationMethod>
-  <AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>
-  <Branch>master</Branch>
-  <LogLevel>info</LogLevel>
-  <SslCertPath></SslCertPath>
-  <SslCertPassword></SslCertPassword>
-  <UrlBase></UrlBase>
-  <InstanceName>Radarr</InstanceName>
-  <UpdateMechanism>Docker</UpdateMechanism>
-</Config>  
-EOF
+  # These lines are not needed when running in a Radarr Docker container
+  # export striptracks_arr_config="config.xml"
+  # cat >$striptracks_arr_config <<EOF
+# <Config>
+  # <BindAddress>*</BindAddress>
+  # <Port>7878</Port>
+  # <SslPort>9898</SslPort>
+  # <EnableSsl>False</EnableSsl>
+  # <LaunchBrowser>True</LaunchBrowser>
+  # <ApiKey>0123456789ABCDEF</ApiKey>
+  # <AuthenticationMethod>Forms</AuthenticationMethod>
+  # <AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>
+  # <Branch>master</Branch>
+  # <LogLevel>info</LogLevel>
+  # <SslCertPath></SslCertPath>
+  # <SslCertPassword></SslCertPassword>
+  # <UrlBase></UrlBase>
+  # <InstanceName>Radarr</InstanceName>
+  # <UpdateMechanism>Docker</UpdateMechanism>
+# </Config>  
+# EOF
   initialize_mode_variables
   fake log :
 }
@@ -49,6 +50,7 @@ test_api_bad_version() {
 }
 
 teardown_suite() {
-  rm -f "$striptracks_arr_config"
+  # This line is not needed when running in a Radarr Docker container
+  # rm -f "$striptracks_arr_config"
   unset radarr_eventtype striptracks_arr_config
 }
