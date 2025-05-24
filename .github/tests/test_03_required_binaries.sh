377f0b8b93993e80d04aa5ba212b7f29347312ff
TheCaptain989 <thecaptain989@protonmail.com>
2025-05-24 01:23:25 +0000

    Container action edit

--- .github/tests/test_03_required_binaries.sh
+++ .github/tests/test_03_required_binaries.sh
@@ -14,15 +14,15 @@ test_binaries_present() {
   assert check_required_binaries
 }
 test_missing_mkvmerge() {
-  sudo mv /usr/bin/mkvmerge /usr/bin/mkvmerge.bak
+  mv /usr/bin/mkvmerge /usr/bin/mkvmerge.bak
   assert_status_code 4 "check_required_binaries 2>/dev/null" && \
   assert_matches "^Error\|/usr/bin/mkvmerge is required" "$(check_required_binaries 2>&1)"
-  sudo mv /usr/bin/mkvmerge.bak /usr/bin/mkvmerge
+  mv /usr/bin/mkvmerge.bak /usr/bin/mkvmerge
 }
 
 test_missing_mkvpropedit() {
-  sudo mv /usr/bin/mkvpropedit /usr/bin/mkvpropedit.bak
+  mv /usr/bin/mkvpropedit /usr/bin/mkvpropedit.bak
   assert_status_code 4 "check_required_binaries 2>/dev/null" && \
   assert_matches "^Error\|/usr/bin/mkvpropedit is required" "$(check_required_binaries 2>&1)"
-  sudo mv /usr/bin/mkvpropedit.bak /usr/bin/mkvpropedit
+  mv /usr/bin/mkvpropedit.bak /usr/bin/mkvpropedit
 }
