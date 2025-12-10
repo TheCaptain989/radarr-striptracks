6f32deea994346eba40c5a101bdd2ef118f4e10d
TheCaptain989 <thecaptain989@protonmail.com>
2025-12-10 18:32:16 +0000

    Linted

--- .github/develop/test_docker.sh
+++ .github/develop/test_docker.sh
@@ -27,7 +27,7 @@ docker exec $container_name /bin/bash -c "test -f /workspaces/$repo/bash_unit"
 bashunit_installed=$?
 docker exec $container_name /bin/bash -c "test -f /usr/bin/mkvmerge"
 mkvtoolnix_installed=$?
-if [ ! bashunit_installed -o ! mkvtoolnix_installed ]; then
+if [ $bashunit_installed -ne 0 -o $mkvtoolnix_installed -ne 0 ]; then
   echo "Installing mkvtoolnix and bash-unit in $container_name container"
   docker exec -it $container_name /bin/bash -c "cd /workspaces/$repo && apk add --no-cache mkvtoolnix && curl -s https://raw.githubusercontent.com/bash-unit/bash_unit/main/install.sh | bash"
   if [ $? -ne 0 ]; then
