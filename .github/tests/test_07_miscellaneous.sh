ddd31c133104074243e87fb8ffedd9db2598bbf3
TheCaptain989 <thecaptain989@protonmail.com>
2025-12-14 16:49:05 -0600

    Release 2.19.0 (MINOR) (#114)
    - New: Added '-f' modifier to --set-default options (issue #113)
    - New unit tests
    - Updated README

--- .github/tests/test_07_miscellaneous.sh
+++ .github/tests/test_07_miscellaneous.sh
@@ -26,3 +26,44 @@ test_org_code_in_default_subtitles() {
   process_org_code subtitles striptracks_default_subtitles
   assert_equals ":eng" "$striptracks_default_subtitles"
 }
+
+test_track_reorder() {
+  export striptracks_json_processed='{"tracks":[{"id":0,"type":"video","language":"und","striptracks_keep":true},{"id":1,"type":"audio","language":"eng","name":"name","striptracks_keep":true},{"id":2,"type":"subtitles","language":"fra","name":"comment forced","forced":true,"striptracks_keep":true},{"id":3,"type":"subtitles","language":"eng","name":"comment","forced":false,"striptracks_keep":true}]}'  
+  export striptracks_audiokeep=":eng"
+  export striptracks_subskeep=":eng:fra"
+  export striptracks_reorder="true"
+  determine_track_order
+  assert_equals "0:0,0:1,0:3,0:2" "$striptracks_neworder"
+}
+
+test_set_default_with_skip_flag() {
+  fake execute_mkv_command :
+  export striptracks_json_processed='{"tracks":[{"id":0,"type":"video","language":"und","striptracks_keep":true},{"id":1,"type":"audio","language":"eng","name":"name","striptracks_keep":true},{"id":2,"type":"subtitles","language":"eng","name":"comment forced","forced":true,"striptracks_keep":true},{"id":3,"type":"subtitles","language":"eng","name":"comment","forced":false,"striptracks_keep":true}]}'
+  export striptracks_default_subtitles=":eng-f"
+  set_default_tracks
+  assert_matches '--edit track:4 --set flag-default=1 --edit track:3 --set flag-default=0' "$striptracks_default_flags"
+}
+
+test_set_default_with_name() {
+  fake execute_mkv_command :
+  export striptracks_json_processed='{"tracks":[{"id":0,"type":"video","language":"und","striptracks_keep":true},{"id":1,"type":"audio","language":"eng","name":"name","striptracks_keep":true},{"id":2,"type":"subtitles","language":"eng","name":"comment forced","forced":true,"striptracks_keep":true},{"id":3,"type":"subtitles","language":"eng","name":"comment","forced":false,"striptracks_keep":true}]}'
+  export striptracks_default_subtitles=":eng=comment"
+  set_default_tracks
+  assert_matches '--edit track:3 --set flag-default=1 --edit track:4 --set flag-default=0' "$striptracks_default_flags"
+}
+
+test_set_default_with_name_and_skip() {
+  fake execute_mkv_command :
+  export striptracks_json_processed='{"tracks":[{"id":0,"type":"video","language":"und","striptracks_keep":true},{"id":1,"type":"audio","language":"eng","name":"name","striptracks_keep":true},{"id":2,"type":"subtitles","language":"eng","name":"comment forced","forced":true,"striptracks_keep":true},{"id":3,"type":"subtitles","language":"eng","name":"comment","forced":false,"striptracks_keep":true}]}'
+  export striptracks_default_subtitles=":eng=comment-f"
+  set_default_tracks
+  assert_matches '--edit track:4 --set flag-default=1 --edit track:3 --set flag-default=0' "$striptracks_default_flags"
+}
+
+test_set_default_with_name_and_skip_reverse() {
+  fake execute_mkv_command :
+  export striptracks_json_processed='{"tracks":[{"id":0,"type":"video","language":"und","striptracks_keep":true},{"id":1,"type":"audio","language":"eng","name":"name","striptracks_keep":true},{"id":2,"type":"subtitles","language":"eng","name":"comment forced","forced":true,"striptracks_keep":true},{"id":3,"type":"subtitles","language":"eng","name":"comment","forced":false,"striptracks_keep":true}]}'
+  export striptracks_default_subtitles=":eng-f=comment"
+  set_default_tracks
+  assert_matches '--edit track:4 --set flag-default=1 --edit track:3 --set flag-default=0' "$striptracks_default_flags"
+}
